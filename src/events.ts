2490dce33606847aecee2c1e56518115b17fff27
Elian Gonzalez <eliangonzalez@MacBook-Air-de-Elian.local>
2023-03-06 16:59:16 +0100

    testing onTileClick and onRestart Functions

--- src/events.ts
+++ src/events.ts
@@ -1,27 +1,60 @@
 import { Player, TileOwner } from "./types";
 import { setTile, setTurn, setWinner } from "./UI/state";
+import { Board } from "./solucion/board";
+import { Tile } from "./solucion/tile";
 
 let turn: Player = "blue";
 
+let board = new Board();
+let clickedTiles: Tile[] = [];
 /**
  * Called when the user clicks on a tile
  * @param row of the clicked tile
  * @param column of the clicked tile
  * @param owner of the clicked tile
  */
-export function onTileClick(row: number, column: number, owner: TileOwner) {
-  console.log(`row: ${row} column: ${column} owner: ${owner}`);
-  setTile(row, column, "none");
-  setWinner("blue");
-  turn = turn === "blue" ? "red" : "blue";
-  setTurn(turn);
+export function onTileClick(row: number, column: number, tileOwner: TileOwner) {
+  console.log(`row: ${row} column: ${column} owner: ${tileOwner}`);
+  let tile = board.tiles[row][column];
+  if (clickedTiles.length !== 0) {
+    if (tile.owner === "none") {
+      console.log("prueba");
+      // let color = tileOwner.split(" ")[0]
+      // if (clickedTiles[0].owner.color == turn) {
+      let selectTile = clickedTiles[0];
+      let [x, y] = selectTile.position;
+      console.log(selectTile);
+      setTile(x, y, "none");
+      let type: TileOwner = selectTile.mapTileOwner();
+      setTile(row, column, type);
+      tile.owner = selectTile.owner;
+      selectTile.owner = "none";
+      clickedTiles.pop();
+      console.log("pop");
+      return;
+    } else {
+      clickedTiles.pop();
+      console.log("pop");
+      return;
+    }
+    // }
+  }
+  if (tile.owner !== "none" && clickedTiles.length === 0) {
+    clickedTiles.push(tile);
+    console.log("push");
+
+    console.log(clickedTiles);
+    console.log(clickedTiles.length);
+  }
 }
 
 /**
  * Called when the user clicks on the "restart" button
  */
 export function onRestart() {
-  setWinner(undefined);
-  setTurn(undefined);
-  setTile(0, 0, "blue queen");
+  board = new Board();
+  board.drawBoard();
+  // setWinner(undefined);
+  // setTurn(undefined);
+  // setTile(0, 0, "blue queen");
 }
