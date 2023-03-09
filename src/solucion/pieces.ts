f76a9a779afa6af535a7d0e2f83ce85a89245681
Elian Gonzalez <eliangonzalez@MacBook-Air-de-Elian.local>
2023-03-09 01:17:34 +0100

    checking movements and attacks

--- src/solucion/pieces.ts
+++ src/solucion/pieces.ts
@@ -1,5 +1,6 @@
 import { Color, Position, PieceType } from "./types";
 import { Tile } from "./tile";
+import { Board } from "./board";
 export abstract class Piece {
   constructor(
     public color: Color,
@@ -7,6 +8,7 @@ export abstract class Piece {
     private type: PieceType
   ) {}
   public abstract canMove(toTile: Tile): boolean;
+  public abstract canAttack(toTile: Tile, middleTiles: Tile): boolean;
   public getType(): PieceType {
     return this.type;
   }
@@ -17,14 +19,39 @@ export class Pawn extends Piece {
     super(color, position, "pawn");
   }
   canMove(toTile: Tile): boolean {
-    const direction: boolean =
-      this.color === "blue"
-        ? toTile.position[1] < this.position[1]
-        : toTile.position[1] > this.position[1];
-    if (direction) {
+    const [x, y] = this.position;
+    const [to_x, to_y] = toTile.position;
+    const diffx = Math.abs(x - to_x);
+    const diffy = Math.abs(y - to_y);
+    if (to_x > 7 || to_x < 0 || to_y < 0 || to_y > 7) {
       return false;
     }
     if (toTile.owner !== "none") {
+      return false;
+    }
+    const direction: boolean =
+      this.color === "blue"
+        ? toTile.position[0] < this.position[0]
+        : toTile.position[0] > this.position[0];
+    if (direction) {
+      return false;
+    }
+    if (x === to_x || y === to_y) {
+      return false;
+    }
+    if (diffx !== diffy) {
+      return false;
+    }
+    return true;
+  }
+  canAttack(toTile: Tile, middleTile: Tile): boolean {
+    if (!this.canMove(toTile)) {
+      return false;
+    }
+    if (middleTile.owner === "none") {
+      return false;
+    }
+    if (middleTile.owner.color === this.color) {
       return false;
     }
     return true;
@@ -36,11 +63,36 @@ export class Queen extends Piece {
     super(color, position, "queen");
   }
   canMove(toTile: Tile): boolean {
-    return false;
+    const [x, y] = this.position;
+    const [to_x, to_y] = toTile.position;
+    const diffx = Math.abs(x - to_x);
+    const diffy = Math.abs(y - to_y);
+    if (to_x > 7 || to_x < 0 || to_y < 0 || to_y > 7) {
+      return false;
+    }
+    if (toTile.owner !== "none") {
+      return false;
+    }
+    if (x === to_x || y === to_y) {
+      return false;
+    }
+    if (diffx !== diffy) {
+      return false;
+    }
+    return true;
+  }
+
+  canAttack(toTile: Tile, middleTile: Tile): boolean {
+    if (!this.canMove(toTile)) {
+      return false;
+    }
+    if (middleTile.owner === "none") {
+      return false;
+    }
+    if (middleTile.owner.color === this.color) {
+      return false;
+    }
+    return true;
   }
 }
 
-// let p1 = new Pawn("blue", [0, 0]);
-// let q1 = new Queen("blue", [0, 0]);
-// let list: Piece[] = [p1, p1, q1];
-// list.map((p) => console.log(p.getType()));
