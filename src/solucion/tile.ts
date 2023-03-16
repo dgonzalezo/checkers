1e7f9c071ee61255df537676df74d1baf2a938fe
Elian Gonzalez <eliangonzalez@MacBook-Air-de-Elian.local>
2023-03-16 02:03:46 +0100

    multi jump second version, creating a class for multiJump

--- src/solucion/tile.ts
+++ src/solucion/tile.ts
@@ -2,17 +2,13 @@ import { setTile } from "../UI/state";
 import { Position, Owner } from "./types";
 import { TileOwner } from "../types";
 import { Board } from "./board";
-import { Pawn } from "./pieces";
+import { MultiJupm } from "./multiJump";
 
 export class Tile {
-  // to store Tiles which selected Tile can attack in multi jump
-  public attackChild: Tile[];
-  // to store Tiles parent where child Tile came from in multi jump
-  public attackParent: Tile[];
+  public multiJump: MultiJupm | null;
 
   constructor(public position: Position, public owner: Owner) {
-    this.attackChild = [];
-    this.attackParent = [];
+    this.multiJump = null;
   }
   public mapTileOwner() {
     let type: TileOwner =
@@ -52,11 +48,12 @@ export class Tile {
           let canAttack = false;
           let middleTile = this.calculateMiddle(toTile, board);
           if (board.allowMultipleJumps) {
-            let canAttack = this.calculateMultipleJumps(this, toTile, board);
+            this.multiJump = new MultiJupm(this)
+            let canAttack = this.multiJump.calculateMultipleJumps(this, toTile, board);
 
             if (canAttack) {
-              let path = this.calculatePath(toTile);
-              this.attackMultiJumpDraw(path, board);
+              let path = this.multiJump.calculatePath(toTile);
+              this.multiJump.attackMultiJumpDraw(path, board);
             }
 
             return canAttack;
@@ -97,11 +94,13 @@ export class Tile {
       }
       return canAttack;
     } else if (board.allowMultipleJumps) {
-      let canAttack = this.calculateMultipleJumps(this, toTile, board);
-
+      this.multiJump = new MultiJupm(this)
+      
+      let canAttack = this.multiJump.calculateMultipleJumps(this, toTile, board);
+      
       if (canAttack) {
-        let path = this.calculatePath(toTile);
-        this.attackMultiJumpDraw(path, board);
+        let path = this.multiJump.calculatePath(toTile);
+        this.multiJump.attackMultiJumpDraw(path, board);
       }
       return canAttack;
     } else {
@@ -201,167 +200,5 @@ export class Tile {
       }
     }
     return canAttackTiles;
-  }
-
-  // Multi Jump Functionality section
-  /**
-   * calculateMultipleJumps
-   */
-  public calculateMultipleJumps(fromTile: Tile, toTile: Tile, board: Board) {
-    if (toTile.owner !== "none") {
-      return false;
-    }
-    this.emptyAttacked(board);
-    let openTiles: Tile[] = [];
-    openTiles.push(fromTile);
-
-    while (openTiles.length) {
-      let tileParent = openTiles.shift()!;
-      let jumps = tileParent.possibleMultiJumps(fromTile, board);
-
-      for (let i = 0; i < jumps.length; i++) {
-        let childTile = jumps[i];
-        if (
-          !tileParent.attackChild.some(
-            (tile) =>
-              tile.position[0] === childTile.position[0] &&
-              tile.position[1] === childTile.position[1]
-          )
-        ) {
-          openTiles.push(jumps[i]);
-          childTile.attackParent.push(tileParent);
-          tileParent.attackChild.push(childTile);
-          if (
-            !fromTile.attackChild.some(
-              (tile) =>
-                tile.position[0] === childTile.position[0] &&
-                tile.position[1] === childTile.position[1]
-            )
-          ) {
-            // adding all tiles that fromTile can attack to search toTile only in fromTile and not child by child
-            fromTile.attackChild.push(childTile);
-          }
-        }
-      }
-    }
-    if (
-      fromTile.attackChild.some(
-        (tile) =>
-          tile.position[0] === toTile.position[0] &&
-          tile.position[1] === toTile.position[1]
-      )
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  public possibleMultiJumps(formTile: Tile, board: Board) {
-    let possibleMovements: Position[] = [
-      [this.position[0] + 2, this.position[1] + 2],
-      [this.position[0] - 2, this.position[1] - 2],
-      [this.position[0] + 2, this.position[1] - 2],
-      [this.position[0] - 2, this.position[1] + 2],
-    ];
-    let possibleAttacks: Tile[] = [];
-    for (let i = 0; i < possibleMovements.length; i++) {
-      let [x, y] = possibleMovements[i];
-      if (x < 8 && x >= 0 && y >= 0 && y < 8) {
-        let toTile = board.tiles[x][y];
-        let middleTile = this.calculateMiddle(toTile, board);
-        if (this.canMultiJump(formTile, toTile, middleTile, board)) {
-          possibleAttacks.push(board.tiles[x][y]);
-        }
-      }
-    }
-
-    return possibleAttacks;
-  }
-  /**
-   * canMultiJump
-   * Calculating if tile can make a multi jump here because I can't access to Methods
-   * canAttack or canMove from Piece class, and it work in a different way
-   * in calculate direction I'm using this, which is refered to the parent tile
-   */
-  public canMultiJump(
-    fromTile: Tile,
-    toTile: Tile,
-    middleTile: Tile,
-    board: Board
-  ) {
-    const [to_x, to_y] = toTile.position;
-    let parent: Tile = fromTile;
-
-    if (to_x > 7 || to_x < 0 || to_y < 0 || to_y > 7) {
-      return false;
-    }
-    if (toTile.owner !== "none") {
-      return false;
-    }
-    // using this Tile to get the current node and not the parent node
-    // if we use the parent node the direction will be allways correct
-    const direction: boolean =
-      board.turn === "blue"
-        ? toTile.position[0] < this.position[0]
-        : toTile.position[0] > this.position[0];
-
-    // check direction only if is pawn with super Pawn false
-    if (
-      parent.owner !== "none" &&
-      parent.owner instanceof Pawn &&
-      !parent.owner.getsuperPawn() &&
-      direction
-    ) {
-      return false;
-    }
-
-    if (middleTile.owner === "none") {
-      return false;
-    }
-    if (middleTile.owner.color === board.turn) {
-      return false;
-    }
-    return true;
-  }
-  /**
-   * calculatePath
-   */
-  public calculatePath(toTile: Tile): Tile[] {
-    // getting parent with shift() to get the first parent added
-    let path: Tile[] = [];
-    path.push(toTile);
-    let parent = toTile.attackParent.shift();
-    while (parent) {
-      path.push(parent);
-      parent = parent.attackParent.shift();
-    }
-
-    return path;
-  }
-  /**
-   * emptyAttacked
-   * Method to empty attackChild and attackParent before every calculateMultipleJumps
-   */
-  public emptyAttacked(board: Board) {
-    for (let i = 0; i < board.size; i++) {
-      for (let j = 0; j < board.size; j++) {
-        let tile = board.tiles[i][j];
-        tile.attackParent = [];
-        tile.attackChild = [];
-      }
-    }
-  }
-  /**
-   * attackMultiJumpDraw
-   */
-  public attackMultiJumpDraw(path: Tile[], board: Board) {
-    let openPath = path;
-    while (openPath.length - 1) {
-      let currentTile = openPath.pop();
-      let nexTile = openPath[openPath.length - 1];
-      let middleTile = currentTile!.calculateMiddle(nexTile, board);
-      this.checkAttackQueen(middleTile, board);
-      currentTile?.attackPieceDraw(nexTile, middleTile);
-    }
   }
 }
